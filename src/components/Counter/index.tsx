e362eadaf0f8cc8ac613634eff272779bb640b76
pedro-candido <pedroferreirac@hotmail.com>
2021-09-13 07:22:28 -0300

    build: renaming selector file

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { selectIsClosed, selectTotal } from "../../store/reducers/selectors";
+import { selectIsClosed, selectTotal } from "../../store/reducers/selector";
 import { CounterContainer, CounterStyle } from "./style";
 
 export const Counter = () => {
